156e8042cd917d2710462631115338d53a191437
IBRAR HUSSAIN <121698755+MrIbrar@users.noreply.github.com>
2025-04-26 19:13:09 +0500

    Python Class 01 Questions Solution

---
+++ main.py
@@ -0,0 +1,29 @@
+#       Lecture no 01 practice Questions
+
+# Write a Program to input two numbers and print there sum.
+
+num1 = float(input("Enter the First Number: "))
+num2 = float(input("Enter the Second Number: "))
+print("The first number is: ",num1, "\nThe second number is: ", num2)
+print(num1+num2)
+
+
+# Write a Program to input Side of a square & Print it area.
+side1 = float(input("Enter the first number: "))
+side2 = float(input("Enter the second number: "))
+area = side1*side2
+print("Area of the Square is: ",area)
+
+
+# Write a Program to input two floaating point numbers and print there average.
+num1 = float(input("Enter the First Number: "))
+num2 = float(input("Enter the Second Number: "))
+print("The first number is: ",num1, "\nThe second number is: ", num2)
+print((num1+num2)/2)
+
+# Write a Program to input two integer point numbers a & b.
+# Print True if a is grater than or equal to b. if not print false.
+a = int(input("Enter the First Number: "))
+b = int(input("Enter the Second Number: "))
+print("The first number is: ",a, "\nThe second number is: ", b)
+print(a >= b)
